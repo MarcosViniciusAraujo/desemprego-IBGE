1ed39839164fde0ff3ca424afc40e67de191f6a0
MarcosViniciusAraujo <m.v.araujo.almeida@gmail.com>
2023-01-10 14:12:53 -0300

    Adicionando documentação

--- desemprego.py
+++ desemprego.py
@@ -6,18 +6,23 @@ API = 'https://apisidra.ibge.gov.br/values/t/6381/n1/all/v/4099/p/all/d/v4099%20
 
 
 def main():
+
+    # Chamando a API
     req = requests.get(API)
     raw = req.json()
 
+    # Separação de colunas
     nivelDesemprego = [float(tri['V']) for tri in raw[1:]]
     trimestrMovel = [tri['D3N'] for tri in raw[1:]]
 
+    # Criação do Dataframe
     dfDesempregoTri = pd.DataFrame(
             nivelDesemprego, 
             index=trimestrMovel, 
             columns=['Desemprego']
         )
 
+    # Exibição do gráfico
     dfDesempregoTri.plot.line()
     plt.xticks(rotation='vertical')
     plt.ylabel('Taxa de desemprego - (%)')
